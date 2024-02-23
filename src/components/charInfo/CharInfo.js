f4fac9ea2ab75214c305787e76179c32a9a98cf9
LDR <89.freedom@gmail.com>
2024-02-23 16:42:17 +0600

    change CharInfo

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -80,10 +80,19 @@ class CharInfo extends Component {
 
 const View = ({ char }) => {
     const { name, description, thumbnail, homepage, wiki, comics } = char;
+
+    let imgStyle = null;
+    if (
+        thumbnail ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+    ) {
+        imgStyle = { objectFit: "unset" };
+    }
+
     return (
         <>
             <div className="char__basics">
-                <img src={thumbnail} alt={name} />
+                <img src={thumbnail} alt={name} style={imgStyle} />
                 <div>
                     <div className="char__info-name">{name}</div>
                     <div className="char__btns">
@@ -99,7 +108,12 @@ const View = ({ char }) => {
             <div className="char__descr">{description}</div>
             <div className="char__comics">Comics:</div>
             <ul className="char__comics-list">
+                {comics.length > 0
+                    ? null
+                    : "There is no comics for this character"}
                 {comics.map((item, i) => {
+                    // eslint-disable-next-line array-callback-return
+                    if (i > 9) return;
                     return (
                         <li key={i} className="char__comics-item">
                             {item.name}
