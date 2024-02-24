2a3a3b263fee06bd3e5bad7d8e6d9c49e23f9813
LDR <89.freedom@gmail.com>
2024-02-24 14:59:47 +0600

    ability to load additional characters at the touch of a button'

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import { Component } from "react";
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
