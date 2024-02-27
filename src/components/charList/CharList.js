2b62848345a9afe121b2ae97e756ab0ce5a536b2
LDR <89.freedom@gmail.com>
2024-02-27 17:25:01 +0600

    add element focus and keyboard control

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -61,10 +61,31 @@ class CharList extends Component {
         });
     };
 
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    };
+
+    focusOnItem = (id) => {
+        // Я реализовал вариант чуть сложнее, и с классом и с фокусом
+        // Но в теории можно оставить только фокус, и его в стилях использовать вместо класса
+        // На самом деле, решение с css-классом можно сделать, вынеся персонажа
+        // в отдельный компонент. Но кода будет больше, появится новое состояние
+        // и не факт, что мы выиграем по оптимизации за счет бОльшего кол-ва элементов
+
+        // По возможности, не злоупотребляйте рефами, только в крайних случаях
+        this.itemRefs.forEach((item) =>
+            item.classList.remove("char__item_selected")
+        );
+        this.itemRefs[id].classList.add("char__item_selected");
+        this.itemRefs[id].focus();
+    };
+
     // Этот метод создан для оптимизации,
     // чтобы не помещать такую конструкцию в метод render
     renderItems(arr) {
-        const items = arr.map((item) => {
+        const items = arr.map((item, i) => {
             let imgStyle = { objectFit: "cover" };
             if (
                 item.thumbnail ===
@@ -76,8 +97,20 @@ class CharList extends Component {
             return (
                 <li
                     className="char__item"
+                    tabIndex={0}
+                    ref={this.setRef}
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}
+                    onKeyDown={(e) => {
+                        if (e.key === " " || e.key === "Enter") {
+                            e.preventDefault();
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}
                 >
                     <img
                         src={item.thumbnail}
