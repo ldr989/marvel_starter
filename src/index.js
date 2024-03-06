16fb0797708e24e54b77c5ca9c021c249cccf4ce
LDR <89.freedom@gmail.com>
2024-03-06 16:31:07 +0600

    add page transitions

--- src/index.js
+++ src/index.js
@@ -5,4 +5,9 @@ import "./style/style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+    <App />
+    // <React.StrictMode>
+    //     <App />
+    // </React.StrictMode>
+);
