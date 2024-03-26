ac9222fe304701e0e456eff605e21b94df95408e
LDR <89.freedom@gmail.com>
2024-03-26 16:49:11 +0600

    fix active class bug in CharList.js and finish the project

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
 import PropTypes from "prop-types";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
@@ -109,13 +109,18 @@ const CharList = (props) => {
         );
     }
 
+    const elements = useMemo(() => {
+        return setContentForlists(
+            currentProcess,
+            () => renderItems(charList),
+            newItemLoading
+        );
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentProcess]);
+
     return (
         <div className="char__list">
-            {setContentForlists(
-                currentProcess,
-                () => renderItems(charList),
-                newItemLoading
-            )}
+            {elements}
             <button
                 className="button button__main button__long"
                 disabled={newItemLoading}
